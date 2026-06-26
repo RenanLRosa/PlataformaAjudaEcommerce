900be9f217d5878e62d88422ae8ffa67ee9fd0a2
Manoela Melo <158612978+ManoelaMeloOliv@users.noreply.github.com>
2026-06-26 11:52:48 -0300

    deletando

--- FeramentaUpdated/src/components/common/EmptyState.jsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyState({ children = 'Nenhum item encontrado.', wide = false }) {
-  return (
-    <div className={`text-center py-7 px-3.5 text-neutral-500 text-sm ${wide ? 'col-span-full' : ''}`}>
-      {children}
-    </div>
-  );
-}
--- FeramentaUpdated/src/utils/linkShare.js
+++
@@ -1,8 +0,0 @@
-/** Monta a mensagem de compartilhamento de um link, substituindo [[NOME]] e [[URL]] */
-export function buildLinkShareMessage(link) {
-  const hasCustomMsg = link.msg && link.msg.trim();
-  if (hasCustomMsg) {
-    return link.msg.replaceAll('[[NOME]]', link.name).replaceAll('[[URL]]', link.url);
-  }
-  return `Esse é o link para ${link.name}:\n\n${link.url}`;
-}
